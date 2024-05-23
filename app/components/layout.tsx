5f49b0f6f8e461eb9c3e74d8a8ba7ed4b5a2f914
Itallo Kavin <italo.keven50@gmail.com>
2024-05-23 00:58:25 -0300

    layout

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -6,9 +6,9 @@ export default function Layout({children,}: Readonly<{children: React.ReactNode;
     return(
         <main className="w-full min-h-screen bg-[#eeeeee] flex max-lg:flex-col"> 
             <Sidebar />
-            <div className="w-[calc(100%-260px)] max-lg:w-full flex flex-col bg-[#eeeeee]">
+            <div className="flex-1 ml-[260px] max-lg:ml-0 flex flex-col bg-[#eeeeee]">
                 <NavBar />
-                <div className="">
+                <div className="flex-1 p-6 overflow-auto max-lg:mt-6">
                     {children}
                 </div>
             </div>

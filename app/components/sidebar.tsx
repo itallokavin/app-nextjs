e24f23ef9c90afab305e4f655996366447094eb6
Itallo Kavin <italo.keven50@gmail.com>
2024-05-23 18:01:02 -0300

    update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,17 +1,16 @@
 'use client'
 
-import React, { useState } from "react"
+import React from "react"
 import Image from "next/image"
 import logo from "../../public/assets/images/foguete.png"
-import { FaHouse,FaUsers,FaBars} from "react-icons/fa6";
-import { FaTimes } from "react-icons/fa";
+import { FaHouse,FaUsers} from "react-icons/fa6"
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import {Divider} from "@nextui-org/react";
 
 const Menu = ({icon, label, redirect}:any) => {
     const pathname = usePathname()
-    const isActive = pathname === redirect
+    const isActive = pathname.startsWith(redirect)
 
     return(
         <Link href={redirect} className={`flex items-center gap-6 px-4 py-3 hover:cursor-pointer ${isActive? 'bg-azul text-white rounded-md font-bold' : ''}`}>
@@ -23,9 +22,6 @@ const Menu = ({icon, label, redirect}:any) => {
 }
 
 export default function Sidebar(){
-    const [isMobile, setIsMobile] = useState(false);
-    const toggleMobile = () => {setIsMobile(!isMobile)}
-
     return(
         <>
             <div className="fixed w-[260px] bg-white lg:min-h-screen shadow-[rgba(0,_0,_0,_0.35)_0px_8px_30px] z-[999] max-lg:hidden">
